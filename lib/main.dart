2ff3f711b94fa9260623eb3df890b00a7cef6613
Antoine Passcaud <an.pascaud@gmail.com>
2019-11-04 18:33:25 +0100

    Mise en palce form

--- lib/main.dart
+++ lib/main.dart
@@ -5,37 +5,62 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final appTitle = 'TrouveTonBar.com';
+
     return MaterialApp(
-      title: 'Projet Flutter',
-      theme: ThemeData(
-        primaryColor: Colors.white,
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
+        ),
+        body: MyCustomForm(),
       ),
-      home: RandomWords(),
     );
   }
 }
 
-class PageUnState extends State<RandomWords> {
-  Widget input() {
-    return TextFormField(
-      decoration: InputDecoration(
-          labelText: 'Location'
-      ),
-    );
+// Create a Form widget.
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
   }
+}
+
+class MyCustomFormState extends State<MyCustomForm> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('TrouveTonBar.com'),
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(
+          labelText: 'Location'),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                }
+              },
+              child: Text('Confirmer'),
+            ),
+          ),
+        ],
       ),
-      body: input(),
     );
   }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  PageUnState createState() => PageUnState();
 }
